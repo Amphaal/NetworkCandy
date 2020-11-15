267239eaac6a9b1065c29f02908da91067c65c7c
Amphaal <guillaume.vara@gmail.com>
2020-11-15 19:59:23 +0100

    noob

--- nw-candy/src/ConnectivityManager.cpp
+++ nw-candy/src/ConnectivityManager.cpp
@@ -50,14 +50,11 @@ STDMETHODIMP_(ULONG) NetworkCandy::CMEventHandler::Release() {
 }
 STDMETHODIMP NetworkCandy::CMEventHandler::ConnectivityChanged(NLM_CONNECTIVITY NewConnectivity) {
     //
-    spdlog::info("nw-candy : ConnectivityChanged !");
-
-    //
     auto isConnected = (NewConnectivity & NLM_CONNECTIVITY_IPV6_INTERNET)
                     || (NewConnectivity & NLM_CONNECTIVITY_IPV4_INTERNET);
 
     //
-    if (_hasProcessed || isConnected != _bInternet) {
+    if (!_hasProcessed || isConnected != _bInternet) {
         _bInternet = isConnected;
         _connectivityChanged(isConnected);
         _hasProcessed = true;
